1c0e8bb667aad6a932f614fa466847287b44cc9e
jcptapar05 <jcptapar05@gmail.com>
2023-04-24 21:20:50 +0800

    resolve issue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -37,15 +37,14 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     Dashboard
                                 </NavLink>
-                            </div>
 
-                            <div
-                                class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"
-                            >
                                 <NavLink :href="route('blogs.index')">
                                     Foods
                                 </NavLink>
                             </div>
+
+
+                                
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
